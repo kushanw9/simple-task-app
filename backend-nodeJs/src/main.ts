2c5b88c829404afdc92681623054af682b946869
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-07-03 12:16:53 +0530

    Initiate the back-end
        Installed dependencies
        -express
        -promise-mysql
        Dev dependencies
        -typescript
        -@types/node
        -@types/express
        -src/main.ts

---
+++ backend-nodeJs/src/main.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const app = express();  // It creates a new Express application that represents backend web server.
+const router = express.Router();
+// use to define routes and handlers for specific paths or endpoints within the app.
+                                 // They allow you to modularize and organize your route handling code.
