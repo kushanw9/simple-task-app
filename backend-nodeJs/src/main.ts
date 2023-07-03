a670a1b5dda7cf972534f659976a4bb9f7bd696a
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-07-03 17:55:21 +0530

    Create a database connection
       -use promise-mysql

--- backend-nodeJs/src/main.ts
+++ backend-nodeJs/src/main.ts
@@ -1,6 +1,32 @@
 import express from 'express';
+import mysql, {Pool} from 'promise-mysql';
 
 const app = express();  // It creates a new Express application that represents backend web server.
-const router = express.Router();
-// use to define routes and handlers for specific paths or endpoints within the app.
-                                 // They allow you to modularize and organize your route handling code.
+const router = express.Router();  // Router - work as dispatcher servlet in spring
+let pool: Pool;
+
+initPool();
+async function initPool() {
+  pool= await mysql.createPool({
+        host: 'localhost',
+        port: 3306,
+        database: 'dep10_simple_task_app',
+        user: 'root',
+        password: 'mysql',
+        connectionLimit:5
+    });
+}
+
+
+router.post("/", (req, res) => {
+
+
+});
+router.delete("/", (req, res) => {
+
+
+});
+
+
+app.use("/app/api/v1/tasks", router);
+app.listen(8080, () => console.log("Sever started 8080"));
